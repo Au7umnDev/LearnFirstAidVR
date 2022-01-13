ded4ef45d97f2b193799f293790ccd0cc351de64
Allyxan <allyxan1337@gmail.com>
2022-01-13 15:06:44 +0300

    Made a foundation for the project!
    Locomotion system is ready. Map is somewhat ready. Let's push it to github!

---
+++ FirstAidLearningVR/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+public class MenuManager : MonoBehaviour
+{
+    public XRNode inputSource;
+    public Panel currentPanel = null;
+    public bool gripValue = false;
+
+    private List<Panel> panelHistory = new List<Panel>();
+
+    private void Start()
+    {
+        SetupPanels();
+    }
+
+    private void SetupPanels()
+    {
+        Panel[] panels = GetComponentsInChildren<Panel>();
+
+        foreach (Panel panel in panels)
+            panel.Setup(this);
+
+        currentPanel.Show();
+    }
+
+    private void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        if (device.TryGetFeatureValue(CommonUsages.gripButton, out gripValue) && gripValue)
+            GoToPrevious();
+    }
+
+    public void GoToPrevious()
+    {
+        if(panelHistory.Count == 0)
+        {
+            Application.Quit();
+            return;
+        }
+        int lastIndex = panelHistory.Count - 1;
+        SetCurrent(panelHistory[lastIndex]);
+        panelHistory.RemoveAt(lastIndex);
+    }
+
+    public void SetCurrentWithHistory(Panel newPanel)
+    {
+        panelHistory.Add(currentPanel);
+        SetCurrent(newPanel);
+    }
+
+    private void SetCurrent(Panel newPanel)
+    {
+        currentPanel.Hide();
+
+        currentPanel = newPanel;
+        currentPanel.Show();
+    }
+}
